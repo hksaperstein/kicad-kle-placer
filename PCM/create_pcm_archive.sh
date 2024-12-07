d4b7f3284f53bce7b224aae1e12142cddad267af
Harrison Saperstein <harrison.saperstein@gmail.com>
2024-12-07 00:01:00 -0500

    Modify workflow for hkaperstein

--- PCM/create_pcm_archive.sh
+++ PCM/create_pcm_archive.sh
@@ -42,5 +42,5 @@ echo "Gather data for repo rebuild"
 echo VERSION=$VERSION >> $GITHUB_ENV
 echo DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1) >> $GITHUB_ENV
 echo DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5) >> $GITHUB_ENV
-echo DOWNLOAD_URL="https:\/\/github.com\/zykrah\/kicad-kle-placer\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
+echo DOWNLOAD_URL="https:\/\/github.com\/hksaperstein\/kicad-kle-placer\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
 echo INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1) >> $GITHUB_ENV
